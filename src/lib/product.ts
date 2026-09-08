330632464ccaa59b5735edad68ec92e1b8dce515
Eric Martinez <eric.michael.mtz@gmail.com>
2026-09-08 10:23:58 -0500

    Pin omni-code 0.6.31 for local session recovery

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -40,7 +40,7 @@ export const BUNDLED_PRODUCT: ProductDefinition = {
   packageName: 'omni-code',
   extraIndexUrl: 'https://pypi.fury.io/ericmichael/',
   prog: 'omni',
-  pinnedVersion: '0.6.30',
+  pinnedVersion: '0.6.31',
 };
 
 /** The product this launcher instance hosts. */
