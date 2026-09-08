637c421ae67811088d28ee8ebf8f7c7565f77485
Eric Martinez <eric.michael.mtz@gmail.com>
2026-09-08 07:41:01 -0500

    Pin omni-code 0.6.28

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -40,7 +40,7 @@ export const BUNDLED_PRODUCT: ProductDefinition = {
   packageName: 'omni-code',
   extraIndexUrl: 'https://pypi.fury.io/ericmichael/',
   prog: 'omni',
-  pinnedVersion: '0.6.27',
+  pinnedVersion: '0.6.28',
 };
 
 /** The product this launcher instance hosts. */
